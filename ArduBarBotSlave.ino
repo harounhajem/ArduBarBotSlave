80afb0fcbec21e04a0973913a8605010ca6050f6
haroun <harou@DESKTOP-20SI2TT>
2017-03-18 18:25:59 +0100

    Step1 working

--- ArduBarBotSlave.ino
+++ ArduBarBotSlave.ino
@@ -1,77 +1,264 @@
+#include <flash_efc.h>
+#include <efc.h>
+#include <DueFlashStorage.h>
 #include "Communication.h"
 #include "Ingridients.h"
-#include "Communication.h"
+DueFlashStorage EEProm;		// EEProm on Arduino DUE
 
+// Pinout
 #define ledLamp 2
+
+// Variables
 String readString;
 
-Container myContainers[6];
-CommunicationClass btCommunication;
+// Class declaration
+Container barBotContainer[6];	// Barbot alcohol container
+CommunicationClass btCommunication;  // Bluetooth communication
 
+struct SaveContainer {
+	String Bottle[12];
+};
+SaveContainer getSave;
+Container openContainer[6];
 
 void setup()
 {
 	Serial1.begin(230400);
 	Serial.begin(115200);
-	pinMode(ledLamp, OUTPUT);
-	digitalWrite(ledLamp, LOW);
-	SetMockDATA();
+	//SetMockDATA();
+
+#pragma region SetMock
+
+
+	barBotContainer[0].SetName("Margaritas");
+	barBotContainer[1].SetName("Vodka");
+	barBotContainer[2].SetName("Tequila");
+	barBotContainer[3].SetName("Whiskey");
+	barBotContainer[4].SetName("Braunstein Gylden");
+	barBotContainer[5].SetName("Ekologiska Osterlensnapsar");
+
+	for (short i = 0; i < 6; i++)
+	{
+		barBotContainer[i].SetAmount(random(100, 2500));
+	}
+
+#pragma endregion
+
+
+#pragma region Save Mechanism
+
+
+	// 1. Convert to String
+	SaveContainer varSave{
+		barBotContainer[0].GetName(),
+		String(barBotContainer[0].GetAmount()),
+		barBotContainer[1].GetName(),
+		String(barBotContainer[1].GetAmount()),
+		barBotContainer[2].GetName(),
+		String(barBotContainer[2].GetAmount()),
+		barBotContainer[3].GetName(),
+		String(barBotContainer[3].GetAmount()),
+		barBotContainer[4].GetName(),
+		String(barBotContainer[4].GetAmount()),
+		barBotContainer[5].GetName(),
+		String(barBotContainer[5].GetAmount())
+	};
+
+	// 2. Save
+	byte b2[sizeof(varSave)]; // create byte array to store the struct
+	memcpy(b2, &varSave, sizeof(varSave)); // copy the struct to the byte array
+	EEProm.write(4, b2, sizeof(varSave)); // write byte array to flash at address 4
+#pragma endregion
+
+#pragma region Load Mechanism
+
+
+										  // 3. Load
+	byte* b = EEProm.readAddress(4); // byte array which is read from flash at adress 4
+	getSave;
+	memcpy(&getSave, b, sizeof(SaveContainer)); // copy byte array to temporary struct
+
+
+
+
+												// 4. Convert saved
+	int counter = 0;
+
+	for (size_t i = 0; i < 12; i++)
+	{
+		if (i % 2 == 0)
+		{
+			openContainer[counter].SetName(getSave.Bottle[i]);
+		}
+		else
+		{
+			long x = getSave.Bottle[i].toInt();
+			openContainer[counter].SetAmount(x);
+			counter++;
+		}
+	}
+#pragma endregion
+
 }
 
 unsigned long timerRecive;
 void loop()
 {
-	if (Serial1.available())
+	Serial.print("\n\nConverted: \n");
+	for (size_t i = 0; i < 6; i++)
 	{
-		int msg = Serial1.read();
-		Serial.print("\n\nCommand recived: ");
-		Serial.println(msg);
-		switch (msg)
-		{
-		case 'e':
-			// Send ingridients
-			btCommunication.SendIngridients(myContainers);
-			break;
-		case 'd':
-			// Update ingridients
-			timerRecive = millis();
-			delay(100);
-			do
-			{
-			Communication.DeSerializerWithChecksum("a");
-			} while ((millis() - timerRecive) > 150);
-
-			break;
-		case 'f':
-			// Get Cocktail order
-			Serial.println("Get cocktail command");
-			break;
-		default:
-			Serial.print("Unkown command sent: ");
-			Serial.println(msg);
-			Serial1.read();
-			break;
-		}
+		Serial.print(openContainer[i].GetName());
+		Serial.print("  ");
+		Serial.println(openContainer[i].GetAmount());
+		delay(150);
 	}
+
+	//if (Serial1.available())
+	//{
+	//	int msg = Serial1.read();
+
+
+	//	Serial.print("\n\nCommand recived: ");
+	//	Serial.println(msg);
+
+
+	//	switch (msg)
+	//	{
+	//	case 'e':
+	//		Serial.println("Send ingridients");
+
+	//		// Send ingridients
+	//		btCommunication.SendIngridients(barBotContainer);
+	//		break;
+	//	case 'd':
+	//		Serial.println("Update ingridients");
+
+	//		// Update ingridients
+	//		timerRecive = millis();
+
+	//		// recive ingridient
+	//		delay(100);
+	//		do
+	//		{
+	//		Communication.DeSerializerWithChecksum("a");
+	//		} while ((millis() - timerRecive) > 150);
+
+	//		// update ingridient 
+
+	//		break;
+	//	case 'f':
+	//		// Get Cocktail order
+	//		Serial.println("Get cocktail command");
+	//		break;
+	//	default:
+	//		Serial.print("Unkown command sent: ");
+	//		Serial.println(msg);
+	//		Serial1.read();
+	//		break;
+	//	}
+	//}
+
+
 }
 // SET MOCKDATA
 void SetMockDATA() {
-	if (!myContainers[0].SetName("Margaritas") ||
-		!myContainers[1].SetName("Vodka") ||
-		!myContainers[2].SetName("Tequila") ||
-		!myContainers[3].SetName("Whiskey") ||
-		!myContainers[4].SetName("Braunstein Gylden") ||
-		!myContainers[5].SetName("Ekologiska Osterlensnapsar"))
+	if (!barBotContainer[0].SetName("Margaritas") ||
+		!barBotContainer[1].SetName("Vodka") ||
+		!barBotContainer[2].SetName("Tequila") ||
+		!barBotContainer[3].SetName("Whiskey") ||
+		!barBotContainer[4].SetName("Braunstein Gylden") ||
+		!barBotContainer[5].SetName("Ekologiska Osterlensnapsar"))
 	{
 		Serial.print("naming error");
 	}
 
 	for (short i = 0; i < 6; i++)
 	{
-		myContainers[i].SetAmount(random(100, 2500));
+		barBotContainer[i].SetAmount(random(100, 2500));
 	}
 
 }
+
+//void StoreData() {
+//
+//
+//#pragma region SetMock
+//
+//
+//	barBotContainer[0].SetName("Margaritas");
+//	barBotContainer[1].SetName("Vodka");
+//	barBotContainer[2].SetName("Tequila");
+//	barBotContainer[3].SetName("Whiskey");
+//	barBotContainer[4].SetName("Braunstein Gylden");
+//	barBotContainer[5].SetName("Ekologiska Osterlensnapsar");
+//
+//	for (short i = 0; i < 6; i++)
+//	{
+//		barBotContainer[i].SetAmount(random(100, 2500));
+//	}
+//
+//#pragma endregion
+//
+//	for (size_t i = 0; i < 6; i++)
+//	{
+//		Serial.println(barBotContainer[i].GetName());
+//		Serial.println(barBotContainer[i].GetAmount());
+//	}
+//	// 1. Convert to String
+//	SaveContainer varSave{
+//		barBotContainer[0].GetName(),
+//		String(barBotContainer[0].GetAmount()),
+//		barBotContainer[1].GetName(),
+//		String(barBotContainer[1].GetAmount()),
+//		barBotContainer[2].GetName(),
+//		String(barBotContainer[2].GetAmount()),
+//		barBotContainer[3].GetName(),
+//		String(barBotContainer[3].GetAmount()),
+//		barBotContainer[4].GetName(),
+//		String(barBotContainer[4].GetAmount()),
+//		barBotContainer[5].GetName(),
+//		String(barBotContainer[5].GetAmount())
+//	};
+//
+//	// 2. Save
+//	byte b2[sizeof(varSave)]; // create byte array to store the struct
+//	memcpy(b2, &varSave, sizeof(varSave)); // copy the struct to the byte array
+//	EEProm.write(4, b2, sizeof(varSave)); // write byte array to flash at address 4
+//}
+
+
+
+//void GetStoredData() {
+//
+//	// 3. Get data
+//	byte* b = EEProm.readAddress(4); // byte array which is read from flash at adress 4
+//	getSave;
+//	memcpy(&getSave, b, sizeof(SaveContainer)); // copy byte array to temporary struct
+//
+//
+//	for (size_t i = 0; i < 12; i++)
+//	{
+//		Serial.println(getSave.Bottle[i]);
+//	}
+//
+//	// 4. Format saved data
+//	int counter = 0;
+//
+//	for (size_t i = 0; i < 12; i++)
+//	{
+//		if (i % 2 == 0)
+//		{
+//			savedData[counter].SetName(getSave.Bottle[i]);
+//		}
+//		else
+//		{
+//			int x = getSave.Bottle[i].toInt();
+//			savedData[counter].SetAmount(x);
+//			counter++;
+//		}
+//	}
+//}
+
 
 
 // GET DATA
@@ -166,7 +353,6 @@ void SendMessageOnCommand() {
 	}
 	digitalWrite(ledLamp, LOW);
 }
-
 void SendMessage() {
 
 	// Två första tecknen eller första tecknet försvinner
